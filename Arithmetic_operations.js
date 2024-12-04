ce760a35b49a995000e03a535c6b8028333b7bb5
Akshayjoshi1405 <akshaykondapur1@gmail.com>
2024-12-04 15:14:29 -0500

    assignment

---
+++ Arithmetic_operations.js
@@ -0,0 +1,8 @@
+let a = parseFloat(prompt("Enter the first value"));
+let b = parseFloat(prompt("Enter the second value"));
+
+console.log(`Addition is: ${a + b}`);
+console.log(`Subtraction is: ${a - b}`);
+console.log(`Multiplication is: ${a * b}`);
+console.log(`Division is: ${a / b}`);
+console.log(`Modulo is: ${a % b}`);
