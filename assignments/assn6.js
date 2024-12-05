9752b6952e8c1d4ca4c5800947dc64523007d86b
Bharath Arsen <bharatharsen@Mac.localdomain>
2024-12-05 13:51:02 -0500

    Today class experiments

---
+++ assignments/assn6.js
@@ -0,0 +1,16 @@
+const str = "question please"
+
+let b = new String('99+99');
+
+console.log(typeof(b));
+console.log(typeof(str));
+
+let c = b.valueOf();
+console.log(typeof(c));
+
+
+// After discussion
+
+const d = '3+9';
+
+console.log(eval(d));
