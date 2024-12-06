0832f90ce9dfc44ac46ca42556819645dc98c4fb
chandu1399 <gopichand1398@gmail.com>
2024-12-06 16:32:22 -0500

    Added assignments

---
+++ class notes/assignments/assignment1.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b= 20;
+
+console.log(`addition of ${a} and ${b} is ${a+b}`);
+console.log(`multiplication of ${a} and ${b} is ${a*b}`);
+console.log(`divison of ${a} and ${b} is ${a/b}`);
