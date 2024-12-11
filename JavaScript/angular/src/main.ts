e337d69ac616563ea51a80f36fda9861f5bec101
rudrapatirohith <rudrapatirohith1@gmail.com>
2024-12-11 16:41:07 -0500

    Angular Web Page

--- JavaScript/angular/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig) // to start our applcn using app comp and using app config(config file of applctn level)
-  .catch((err) => console.error(err));
