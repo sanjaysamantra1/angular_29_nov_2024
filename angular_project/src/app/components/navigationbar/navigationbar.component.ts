28be1508c1e2c137a430590352e24e74529247b6
Navya <navyaswe@uoregon.edu>
2024-12-11 16:48:49 -0500

    'day1_angular'

---
+++ angular_project/src/app/components/navigationbar/navigationbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigationbar',
+  imports: [],
+  templateUrl: './navigationbar.component.html',
+  styleUrl: './navigationbar.component.css'
+})
+export class NavigationbarComponent {
+
+}
