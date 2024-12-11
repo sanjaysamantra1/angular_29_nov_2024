9ea894ad846e6341e498be2907476d54018c51eb
kandulavamsi <kvamsi1924@gmail.com>
2024-12-11 15:24:25 -0500

    Anguler project

--- javascript/day_4_dec_2/Arrow_limitation_1.js
+++ javascript/day_4_dec_2/Arrow_limitation_1.js
@@ -1,12 +1,20 @@
+var firstName = 'vamsi';
+var lastName = 'kandula';
+
+
 let user = {
     firstName: 'Sachin',
     lastName: 'Tendulkar',
     fullName1: function () { // Regular : this = how the function is called
+        console.log(this);
         console.log(this.firstName + " " + this.lastName); // this = user
     },
     fullName2: () => { // this = global
-        console.log(this.firstName + this.lastName)
+        console.log(this);
+        console.log(this.firstName + this.lastName);
+        
     }
 }
+
 user.fullName1(); // inside fullName1() ,  this  = user
 user.fullName2();
