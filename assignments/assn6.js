3fecca9472177ae965b9a7c6a0ad588499d554d4
Bharath Arsen <bcgrkofficial@gmail.com>
2024-12-12 09:04:47 -0500

    Strings assgn

--- assignments/assn6.js
+++ assignments/assn6.js
@@ -1,22 +1,3 @@
-// const str = "question please"
-
-// let b = new String('99+99');
-
-// console.log(typeof(b));
-// console.log(typeof(str));
-
-// let c = b.valueOf();
-// console.log(typeof(c));
-
-
-// // After discussion
-
-// const d = '3+9';
-
-// console.log(eval(d));
-
-
-// ----------------------------------TASK - 1---------------------------
 
 //1. Print the occurence of each character in a string
 // input: 'entertainment'
