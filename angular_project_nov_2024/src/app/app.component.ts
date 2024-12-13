f407985c7dfa7a2815d48ce87d7a871ed292b490
Dhanush <dhanush.tdc@gmail.com>
2024-12-13 16:33:25 -0500

    Databinding And Assignment - 1

--- angular_project_nov_2024/src/app/app.component.ts
+++ angular_project_nov_2024/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
@@ -8,7 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CategoriesComponent, CarouselComponent, BodyComponent, FooterComponent],
+  imports: [NavbarComponent, CategoriesComponent, CarouselComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular_project_nov_2024/src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-databinding',
+  imports: [ // modules , directives,components,pipes
+    FormsModule
+  ],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+  myName: string = 'Sachin Tendulkar';
+  img_url: string = 'https://images-cricketcom.imgix.net/sachin-tendulkar-1693132548682?fit=crop&crop=face&auto=format&ixlib=react-9.8.0&h=250&w=180'
+  flag: boolean = false;
+  myWidth = '50%';
+  myHeight = '200px';
+  myAriaLabel = 'Button Label....';
+  num1 = 0;
+  num2 = 0;
+  msg: string = '';
+  selectedState: string = '';
+  selectedOperation: string = '';
+  flip: boolean = false;
+  text : string = 'text';
+  counterValue : number = 0;
+
+
+  f1() {
+    console.log('F1 called.....')
+  }
+  addition(n1: any, n2: any) {
+    console.log(`Addition of ${n1} and ${n2} is ${+n1 + +n2}`)
+  }
+
+  operation(n1: any, n2: any, operator: string) {
+    switch(operator) {
+      case '+':
+      return +n1 + +n2;
+      case "-":
+      return +n1 - +n2;
+      case "*":
+      return +n1 * +n2;
+      case "/":
+      return +n1 / +n2;
+    }
+    return null;
+  }
+
+  clickEvent() {
+    this.flip = !this.flip;
+    console.log(this.flip);
+    this.flip ? this.text= "password" : this.text = "text";
+  }
+
+}
