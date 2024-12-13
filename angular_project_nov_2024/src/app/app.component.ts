8b033ed1b77451e9582ebd1dfc8b67e75ec601cc
kandulavamsi <kvamsi1924@gmail.com>
2024-12-13 12:11:29 -0500

    pusing local changes

--- angular_project_nov_2024/src/app/app.component.ts
+++ angular_project_nov_2024/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    NavbarComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    BodyComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
