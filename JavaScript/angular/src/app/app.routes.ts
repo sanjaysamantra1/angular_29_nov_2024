8d1ac8804b6a3169cef9a509530931959ee2ce6c
rudrapatirohith <rudrapatirohith1@gmail.com>
2024-12-14 01:37:29 -0500

    Angular Assignment

--- JavaScript/angular/src/app/app.routes.ts
+++ JavaScript/angular/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './compoents/login/login.component';
-import { SignupComponent } from './compoents/signup/signup.component';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './compoents/login/login.component'; // Ensure correct path
+import { SignupComponent } from './compoents/signup/signup.component'; // Ensure correct path
 
 export const routes: Routes = [
-    // {path: '', component: AppComponent},
-    {path: 'login' , component: LoginComponent},
-    {path: 'signup', component: SignupComponent},
+//   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default route to Login
+//   { path: 'login', component: LoginComponent }, // Login route
+//   { path: 'signup', component: SignupComponent }, // Signup route
+//   { path: '**', redirectTo: 'login' }, // Wildcard route to handle invalid paths
 ];
--- JavaScript/angular/src/app/compoents/databinding/databinding.component.ts
+++ JavaScript/angular/src/app/compoents/databinding/databinding.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-databinding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './databinding.component.html',
   styleUrl: './databinding.component.css'
 })
 export class DatabindingComponent {
 
+  val = "Hi, Welcome To My Page";
+  flag = false;
+  isPass=false;
+  textLength = 100;
+  text = '';
+  diff = this.textLength - this.text.length;
+  selectedState = "Please Select The State";
+
+
+  val1 = 0;
+  val2 = 0;
+  res = 0;
+
+  operator = "+";
+
+
+  calculate() {
+    switch (this.operator) {
+      case "+":
+        this.res = this.val1 + this.val2;
+        break;
+      case "-":
+        this.res = this.val1 - this.val2;
+        break;
+      case "*":
+        this.res = this.val1 * this.val2;
+        break;
+      case "%":
+        this.res = this.val1 % this.val2;
+        break;
+      default:
+        this.res = 0;
+    }
+  }
+
+  passwordValue = '';
+
+
+  togglePasswordVisibility(){
+    this.isPass=!this.isPass;
+  }
+
+
+
+  counter=0;
+
+  counterr(action: string){
+    if(action==='increment'){
+      this.counter=this.counter+1;
+    }
+    else if(action==='decrement'){
+      this.counter=this.counter-1;
+    }
+    else if(action==='reset'){
+      this.counter=0;
+    }
+  }
 }
