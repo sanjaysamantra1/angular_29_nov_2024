52e896b1a0602fcbb18ea2eca1c43d5e6a2f118c
malursagar <sagar.malur5@gmail.com>
2024-12-14 02:30:21 -0500

    Angular Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    CategoryComponent,
+  CarouselComponent,FooterComponent,BodyComponent,DatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-databinding',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+  // toggling a button
+   isVisible = true;
+   togglePara() {
+     this.isVisible = !this.isVisible;
+   }
+   //text area
+   text: string = '';
+   maxLength: number = 100;
+   remainingChars: number = this.maxLength;
+ 
+   updateRemainingChars() {
+     this.remainingChars = this.maxLength - this.text.length;
+   }
+ 
+   //dropdown
+   states: string[] = ['California', 'Texas', 'New York', 'Florida', 'Illinois'];
+   selectedState: string = '';
+ 
+   onStateChange() {
+     console.log('Selected State:', this.selectedState);
+   }
+ 
+   //calculate
+   input1: number = 0;
+   input2: number = 0;
+   selectedOperation: string = '+';
+   result: number = 0;
+   operations: string[] = ['+', '-', '*', '/'];
+ 
+   calculate() {
+     switch (this.selectedOperation) {
+       case '+':
+         this.result = this.input1 + this.input2;
+         break;
+       case '-':
+         this.result = this.input1 - this.input2;
+         break;
+       case '*':
+         this.result = this.input1 * this.input2;
+         break;
+       case '/':
+         this.result = this.input2 !== 0 ? this.input1 / this.input2 : NaN; // Prevent division by zero
+         break;
+       default:
+         this.result = NaN;
+     }
+   }
+ //toggle text password
+ isPassword: boolean = true; // Initial type is 'password'
+   inputValue: string = '';
+ 
+   togglePasswordVisibility() {
+     this.isPassword = !this.isPassword; // Toggle the boolean
+   }
+ 
+   //3 controls
+   counter: number = 0;
+   increment() {
+     this.counter++;
+   }
+   decrement() {
+     if (this.counter > 0) {
+       this.counter--;
+     }
+   }
+   reset() {
+     this.counter = 0;
+   }
+ 
+ }
