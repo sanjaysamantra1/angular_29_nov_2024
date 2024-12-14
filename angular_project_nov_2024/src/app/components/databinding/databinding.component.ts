c513719a44fd4a15e88803d9f24dd503e44cb001
dheerajmaddula04 <maddula.04dheeraj@gmail.com>
2024-12-14 00:33:37 -0500

    data binding assignment

--- angular_project_nov_2024/src/app/components/databinding/databinding.component.ts
+++ angular_project_nov_2024/src/app/components/databinding/databinding.component.ts
@@ -20,12 +20,36 @@ export class DatabindingComponent {
   num2 = 0;
   msg: string = '';
   selectedState: string = '';
-
+  selectedOperator: string = '';
+  flag1: boolean = false;
+  text: string = 'text';
+  counterValue : number = 0;
 
   f1() {
     console.log('F1 called.....')
   }
+
   addition(n1: any, n2: any) {
     console.log(`Addition of ${n1} and ${n2} is ${+n1 + +n2}`)
   }
+
+  operations(n1: any, n2: any, opr: any) {
+    switch(opr){
+      case 'Add':
+        return +n1 + +n2;
+      case 'Sub':
+        return +n1 - +n2;
+      case 'Mul':
+        return +n1 * +n2;
+      case 'Div':
+        return +n1 / +n2;
+    }
+    return null;
+  }
+
+  clickEvent(){
+    this.flag1 = !this.flag1;
+    this.flag1 ? this.text='password' : this.text='text';
+  }
+  
 }
