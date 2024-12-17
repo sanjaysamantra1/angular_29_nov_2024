3aade98ba5ed719f74256cdf0c8c642fc6e9643e
Navya <122176358+navyaswe@users.noreply.github.com>
2024-12-16 23:47:27 -0500

    assignment_employee

--- angular_project_nov_2024/src/app/app.component.ts
+++ angular_project_nov_2024/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
+//import { CategoriesComponent } from './components/categories/categories.component';
+//import { CarouselComponent } from './components/carousel/carousel.component';
+//import { BodyComponent } from './components/body/body.component';
+//import { FooterComponent } from './components/footer/footer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    CategoriesComponent,
-    CarouselComponent,
-    BodyComponent,
-    FooterComponent
+    EmployeeComponent,
+    //CategoriesComponent,
+    //CarouselComponent,
+    //BodyComponent,
+    //FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
