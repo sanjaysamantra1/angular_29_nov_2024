9441a10579f6d00b50027ae3c18f68aa053c9d55
Pradeep Yendluri <ynvp48@gmail.com>
2024-12-16 23:03:21 -0500

    Directives Assignment

--- angular/day_1_practice/src/app/components/body/body.component.ts
+++ angular/day_1_practice/src/app/components/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { DatabindingComponent } from "../databinding/databinding.component";
+import { DatabindingComponent } from '../databinding/databinding.component';
+import { DirectivesComponent } from '../directives/directives.component';
 
 @Component({
-  selector: 'app-body',
-  imports: [DatabindingComponent],
-  templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+    selector: 'app-body',
+    imports: [DatabindingComponent, DirectivesComponent],
+    templateUrl: './body.component.html',
+    styleUrl: './body.component.css',
 })
 export class BodyComponent {
 
