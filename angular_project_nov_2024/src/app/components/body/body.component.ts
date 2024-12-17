0d7006393e3791250dc01df7eb015eb842c27574
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-12-17 21:03:44 +0530

    pagination

--- angular_project_nov_2024/src/app/components/body/body.component.ts
+++ angular_project_nov_2024/src/app/components/body/body.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DatabindingComponent } from '../databinding/databinding.component';
 import { DirectivesComponent } from '../directives/directives.component';
+import { UserListComponent } from '../user-list/user-list.component';
 
 @Component({
   selector: 'app-body',
   imports: [
-    DatabindingComponent,
-    DirectivesComponent
+    // DatabindingComponent,
+    // DirectivesComponent,
+    UserListComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
