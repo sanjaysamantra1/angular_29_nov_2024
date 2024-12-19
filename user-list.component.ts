616037cfbee2a34d5b67e84bcfd30550eb8b3ccc
srimanth <84217751+srimanth496@users.noreply.github.com>
2024-12-19 11:51:17 -0600

    Add files via upload

---
+++ user-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import * as data from './users.json';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [NgxPaginationModule],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent {
+
+  users = (data as any).default
+
+  p :any;
+}
