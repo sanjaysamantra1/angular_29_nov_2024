2c64faac574259a721737dcaead8f020df80fa4c
Navya <122176358+navyaswe@users.noreply.github.com>
2024-12-19 07:31:35 -0500

    assignment_toggle

--- angular_project_nov_2024/src/app/components/employee/employee.component.ts
+++ angular_project_nov_2024/src/app/components/employee/employee.component.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
+import * as employeeData from './user.json';
+import { Injectable } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [NgxPaginationModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
-//need to take an object array and print the details in table and cards formate
-employee = [
-  { eId: 101, name: 'sanjay', sal: 5000, gender: 'male' },
-  { eId: 104, name: 'geeta', sal: 8000, gender: 'female' },
-  { eId: 103, name: 'sameer', sal: 7000, gender: 'male' },
-  { eId: 102, name: 'sita', sal: 9000, gender: 'female' },
-  { eId: 105, name: 'deepak', sal: 8000, gender: 'male' }
-];
+  employeeList = (employeeData as any).default;
+  isTableView: boolean = true;
+  isDarkMode: boolean = true;
 
+  constructor(private renderer: Renderer2) {}
 
+  toggleTheme() {
+    console.log("Theme toggled");
+    this.isDarkMode = !this.isDarkMode;
+    console.log(this.isDarkMode);
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
 
+  employees = [
+    { id: 101, name: 'Sanjay', salary: 5000, gender: 'Male' },
+    { id: 104, name: 'Geeta', salary: 8000, gender: 'Female' },
+    { id: 103, name: 'Sameer', salary: 7000, gender: 'Male' },
+    { id: 102, name: 'Sita', salary: 9000, gender: 'Female' },
+    { id: 105, name: 'Deepak', salary: 8000, gender: 'Male' }
+  ];
 
-
-
+  currentPage: any;
 }
