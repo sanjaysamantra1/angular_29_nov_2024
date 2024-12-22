cb525d043a68dd5a439fe61a20c4dd40f047611b
sathwik990416 <sathwik.311022@gmail.com>
2024-12-22 11:20:52 -0500

    body changes

--- angular_project_nov_2024/src/app/components/body/body.component.ts
+++ angular_project_nov_2024/src/app/components/body/body.component.ts
@@ -2,17 +2,27 @@ import { Component } from '@angular/core';
 import { DatabindingComponent } from '../databinding/databinding.component';
 import { DirectivesComponent } from '../directives/directives.component';
 import { UserListComponent } from '../user-list/user-list.component';
+import { Demo1Component } from '../demo1/demo1.component';
+import { EmployeeCRUDComponent } from "../employee-crud/employee-crud.component";
+import { PipesComponent } from '../pipes/pipes.component';
+import { ProductsComponent } from "../products/products.component";
 
 @Component({
   selector: 'app-body',
   imports: [
-    // DatabindingComponent,
-    // DirectivesComponent,
-    UserListComponent
-  ],
+    DatabindingComponent,
+    DirectivesComponent,
+    UserListComponent,
+    Demo1Component,
+    EmployeeCRUDComponent, PipesComponent,
+    ProductsComponent,
+    
+],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+
+ 
 
 }
