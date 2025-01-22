48bc91d3f90acf5631986583dd7b3649bd5374d2
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-01-22 21:01:13 +0530

    form demo 1

---
+++ angular_project_nov_2024/src/app/components/form-demo2/form-demo2.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo2',
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: './form-demo2.component.html',
+  styleUrl: './form-demo2.component.css'
+})
+export class FormDemo2Component {
+  user = {
+    firstName: '',
+    lastName: 'Kohli',
+    email: 'Virat@gmail.com',
+    address: {
+      street: "",
+      city: "",
+      pincode: "123456"
+    }
+  }
+  submitMyForm(formData: any) {
+    console.log(formData)
+  }
+  reset(myForm :NgForm) {
+    console.log(myForm)
+    console.log('Do Something...');
+    myForm.resetForm();
+  }
+}
