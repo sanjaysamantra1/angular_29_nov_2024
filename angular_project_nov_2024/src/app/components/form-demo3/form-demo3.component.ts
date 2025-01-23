e7833486dd31a33f0ab1a80a695d6d797f46b0f9
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-01-23 20:48:43 +0530

    dynamic form

---
+++ angular_project_nov_2024/src/app/components/form-demo3/form-demo3.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo3',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  templateUrl: './form-demo3.component.html',
+  styleUrl: './form-demo3.component.css'
+})
+export class FormDemo3Component {
+  registerForm: any;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  ngOnInit() {
+    this.usingFormGroup();
+    this.usingFormBuilder();
+  }
+  usingFormGroup() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('sachin', [Validators.required, Validators.minLength(5)]),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      address: new FormGroup({
+        street: new FormControl(),
+        city: new FormControl(),
+        pincode: new FormControl(),
+      })
+    }, { updateOn: 'blur' });
+  }
+
+  usingFormBuilder() {
+    this.registerForm = this.fb.group({
+      firstName: ['sachin', [Validators.required, Validators.minLength(5)]],
+      lastName: [],
+      email: [],
+      address: this.fb.group({
+        street: [],
+        city: [],
+        pincode: [],
+      })
+    })
+  }
+
+  submitMyForm(formData: any) {
+    console.log(formData)
+    console.log(formData.value)
+  }
+
+  setFormValue() {
+    this.registerForm.setValue({ firstName: 'aaa', lastName: 'bbb' })
+  }
+  patchFormValue() {
+    this.registerForm.patchValue({ firstName: 'aaaa', lastName: 'bbbb' })
+  }
+}
