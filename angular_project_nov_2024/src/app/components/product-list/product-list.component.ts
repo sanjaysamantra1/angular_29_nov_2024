4fb8236cbabdb781dc4b4e35e22aa205899b1204
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-01-28 21:54:26 +0530

    routing

--- angular_project_nov_2024/src/app/components/product-list/product-list.component.ts
+++ angular_project_nov_2024/src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
